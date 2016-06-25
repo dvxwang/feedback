e1c1f0d9103009bc78b512783b03b83072fc4aa6
elizabethkim828 <elizabethkim828@gmail.com>
2016-06-25 15:22:36 -0400

    tweak

--- browser/js/common/question/question.factory.js
+++ browser/js/common/question/question.factory.js
@@ -1,12 +1,10 @@
 app.factory('QuestionFactory', function ($http) {
 
 	var obj = {};
-	var cache = [];
 
 	obj.getAllByLectureId = function(lectureId) {
 		return $http.get('/api/question/lecture/' + lectureId).then(function(res) {
-			angular.copy(res.data, cache);
-			return cache;
+			return res.data;
 		})
 	}
 
