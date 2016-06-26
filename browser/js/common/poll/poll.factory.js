37b8eaa32132d7a8a7c37a9de5f0dc51d7cc4855
Brinn Riordan <brinn.riordan@gmail.com>
2016-06-26 16:10:05 -0400

    directive

--- browser/js/common/poll/poll.factory.js
+++ browser/js/common/poll/poll.factory.js
@@ -1,28 +1,30 @@
 app.factory('PollFactory', ($http) => {
+  function dotData(dot) {
+    return dot.data
+  }
   return {
+    dotData: (dot) => {
+      return dot.data
+    },
     getAllByLectureId: (id) => {
       return $http.get('/api/poll/lecture/'+id)
-      .then((polls) => {
-        return polls.data
-      })
+      .then(dotData)
     },
     getOneByPollId: (id) => {
       return $http.get('/api/poll/'+id)
-      .then((poll) => {
-        return poll.data
-      })
+      .then(dotData)
     },
     createPoll: (pollObj) => {
       return $http.post('/api/poll/', pollObj)
-      .then((newPoll) => {
-        return newPoll.data
-      })
+      .then(dotData)
     },
     updatePoll: (pollObj, id) => {
       return $http.put('/api/poll/'+id, pollObj)
-      .then((updatedPoll) => {
-        return updatedPoll.data
-      })
+      .then(dotData)
+    },
+    deletePoll: (id) => {
+      return $http.delete('/api/poll/'+id)
+      .then(dotData)
     }
   }
 })
