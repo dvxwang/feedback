0d06f803588dddac8e14e631820400c3212f0a8c
Brinn Riordan <brinn.riordan@gmail.com>
2016-06-30 12:17:30 -0400

    instructor view changes

--- browser/js/views/instructor/instructor.controller.js
+++ browser/js/views/instructor/instructor.controller.js
@@ -1,12 +1,14 @@
 app.controller('InstructorCtrl', function ($scope, $log, $state, LectureFactory) {
 
     $(document).ready(function() {
-        
+
         gapi.hangout.render('startButton2', {
         'render': 'createhangout',
         'hangout_type': 'onair',
         'initial_apps': [
-            { app_id : 'effortless-city-135523', start_data : 'dQw4w9WgXcQ', 'app_type' : 'ROOM_APP' }
+            { 'app_id' : 'effortless-city-135523',
+              'start_data' : 'dQw4w9WgXcQ',
+              'app_type' : 'ROOM_APP' }
         ],
         'widget_size': 72
         });
@@ -14,13 +16,15 @@ app.controller('InstructorCtrl', function ($scope, $log, $state, LectureFactory)
         var queue = {
             confused: [],
             great: [],
-            example: []
+            example: [],
+            x: []
         };
 
         var dataQueue = {
             confused: [],
             great: [],
-            example: []
+            example: [],
+            x: []
         };
 
         var dataLength=30;
@@ -42,8 +46,8 @@ app.controller('InstructorCtrl', function ($scope, $log, $state, LectureFactory)
             title :{
                 text: "Live Feedback",
                 fontColor: "white"
-            },  
-            backgroundColor: null,        
+            },
+            backgroundColor: null,
             axisX: {
                 tickLength: 0,
                 valueFormatString: " ",
@@ -59,62 +63,73 @@ app.controller('InstructorCtrl', function ($scope, $log, $state, LectureFactory)
             },
             data: [{
                 markerType: 'none',
-                color: 'orange',
+                color: '#F0AD4E',
                 type: "line",
                 name: "Confused",
                 dataPoints: queue['confused']
             },
             {
                 markerType: 'none',
-                color: 'blue',
+                color: '#5BC0DE',
                 type: "line",
                 name: "Example",
                 dataPoints: queue['example']
             },
             {
                 markerType: 'none',
-                color: 'green',
+                color: '#5CB85C',
                 type: "line",
                 name: "Great",
                 dataPoints: queue['great']
+            },
+            {
+                markerType: 'none',
+                color: 'white',
+                type: 'line',
+                name: 'x-axis',
+                dataPoints: queue['x']
             }
             ]
         });
 
-        var updateChart = function () {    
+        var updateChart = function () {
             xVal++;
-            
             queue['confused'].push({x: xVal, y:0+dataQueue['confused'].length});
             queue['example'].push({x: xVal, y:0+dataQueue['example'].length});
             queue['great'].push({x: xVal, y:0+dataQueue['great'].length});
+            queue['x'].push({x: xVal, y:0})
 
-            if (queue['confused'].length > dataLength) queue['confused'].shift();                
-            if (queue['example'].length > dataLength) queue['example'].shift();  
-            if (queue['great'].length > dataLength) queue['great'].shift();                
 
-            chartCode.render();  
+            if (queue['confused'].length > dataLength) queue['confused'].shift();
+            if (queue['example'].length > dataLength) queue['example'].shift();
+            if (queue['great'].length > dataLength) queue['great'].shift();
+            if (queue['x'].length > dataLength) queue['x'].shift();
 
-            dataQueue['confused']=[];
-            dataQueue['example']=[];
-            dataQueue['great']=[];
+            chartCode.render();
+            if (xVal%15 === 0) {
+              dataQueue['confused'].shift();
+              dataQueue['example'].shift();
+              dataQueue['great'].shift();
+            }
 
         };
 
         function updateInstructorView(){
             setInterval(function(){
                 updateChart();
-            }, 1000); 
+            }, 1000);
         };
 
         updateInstructorView();
 
         socket.on('updateFeedback', function (data) {
           data = data.toLowerCase();
+          console.log(data)
           dataQueue[data].push("instance");
         });
 
         $('.start').click(function(){
-            
+
             if ($(this).html()=='Begin') {
                 LectureFactory.setStart().then(function(lecture) {
                     $scope.curLecture = lecture;
