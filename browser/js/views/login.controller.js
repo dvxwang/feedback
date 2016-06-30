cfd4c62b154b09ee8500f0159acef60abeb226d6
Brinn Riordan <brinn.riordan@gmail.com>
2016-06-30 17:12:40 -0400

    login refactor

--- browser/js/views/login.controller.js
+++ browser/js/views/login.controller.js
@@ -3,19 +3,32 @@ app.controller('LoginCtrl', function ($scope, $state) {
 	$scope.loginStatus = function(){
 		var temp = $scope.login.toLowerCase();
 
-		if (temp==='admin'){
-			$state.go('admin');
+		switch (temp) {
+			case 'admin':
+				$state.go('admin');
+				break;
+			case 'student':
+				$state.go('student');
+				break;
+			case 'summary':
+				$state.go('summary');
+				break;
+			default:
+				$scope.login = "";
+				$("input").attr("placeholder", "Please input valid credential");
 		}
-		else if (temp==='student'){
-			$state.go('student');
-		}
-        else if (temp==='summary'){
-            $state.go('summary');
-        }
-        else {
-            $scope.login = "";
-            $("input").attr("placeholder", "Please input valid credential");            
-        }
+
+		// if (temp==='admin'){
+		// 	$state.go('admin');
+		// } else if (temp==='student'){
+		// 	$state.go('student');
+		// } else if (temp==='summary'){
+    //   $state.go('summary');
+    // } else {
+    //   $scope.login = "";
+    //   $("input").attr("placeholder", "Please input valid credential");
+    // }
+
 	}
 
 });
