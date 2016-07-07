8142c42401fc91b3b429c3a4705037ca4b9d4963
elizabethkim828 <elizabethkim828@gmail.com>
2016-07-07 11:16:15 -0400

    refactored question directive

--- browser/js/common/question/question.directive.js
+++ browser/js/common/question/question.directive.js
@@ -9,10 +9,56 @@ app.directive('question', function($state, QuestionFactory, LectureFactory) {
         link: function(scope, element, attrs) {
             scope.lecture = scope.$parent.curLecture
 
-            QuestionFactory.getAllByLectureId(scope.lecture.id).then(function(questions) {
+            QuestionFactory.getAllByLectureId(scope.lecture.id)
+            .then(function(questions) {
                 scope.questions = questions.filter(function(q) { return q.status === 'open' }).reverse()
             })
 
+            scope.submit = submit;
+            scope.delete = deleteQuestion;
+            scope.close = close;
+            scope.move = emitMove;
+            scope.upvote = upvote;
+            scope.downvote = downvote;
+
+            // event listeners
+
+            socket.on('addQuestion', renderAddQuestion);
+            socket.on('deleteQuestion', renderDeleteQuestion);
+            socket.on('receivedUpvote', renderUpvote);
+            socket.on('receivedDownvote', renderDownvote);
+            socket.on('moving', renderMoveQuestion);
+
+            // scope-related function declarations
+
+            function upvote(question) {
+                question.hasUpvoted = !question.hasUpvoted;
+                socket.emit('upvoting', question);
+                return QuestionFactory.update(question.id, { upvotes: question.upvotes+1 });
+            }
+
+            function downvote(question) {
+                question.hasUpvoted = !question.hasUpvoted;
+                socket.emit('downvoting', question);
+                return QuestionFactory.update(question.id, { upvotes: question.upvotes-1 });
+            }
+
+            function close(question) { return QuestionFactory.update(question.id, { status: 'closed'}) };
+            
+            function emitMove(question, n) { socket.emit('move', question, n) };
+            
+            function deleteQuestion(question) { return QuestionFactory.delete(question) };
+            
+            function submit() {
+                if (scope.newQuestion) {
+                    var question = { text: scope.newQuestion, submitTime: Date.now(), upvotes: 0, lectureId: scope.lecture.id };
+                    return QuestionFactory.store(question).then(function(q) {
+                        scope.newQuestion = null;
+                    })
+                }
+            }
+
+            // helper functions
             function findIndex(question) {
                 for (var i = 0; i < scope.questions.length; i++) {
                     if (scope.questions[i].text === question.text) {
@@ -22,79 +68,43 @@ app.directive('question', function($state, QuestionFactory, LectureFactory) {
                 return -1;
             }
 
-            function move(question, n) {
+            // event-listener function declarations
+
+            function rerender() { scope.$evalAsync() };
+            
+            function renderMoveQuestion(question, n) {
                 var index = findIndex(question);
                 if (index+n > -1 && index+n < scope.questions.length) {
                     scope.questions.splice(index, 1);
                     scope.questions.splice(index+n, 0, question);
                 }
-            }
-
-            scope.submit = function() {
-                if (scope.newQuestion) {
-                    var question = {text: scope.newQuestion, submitTime: Date.now(), upvotes: 0, lectureId: scope.lecture.id};
-                    return QuestionFactory.store(question).then(function(q) {
-                        // socket.emit('addingQuestion', q);
-                        scope.newQuestion = null;
-                    })
-                }
-            }
-
-            scope.delete = function(question) {
-                // socket.emit('deletingQuestion', question);
-                return QuestionFactory.delete(question);
-            }
-
-            scope.close = function(question) {
-                return QuestionFactory.update(question.id, { status: 'closed'});
-            }
-
-            scope.move = function(question, n) {
-                socket.emit('move', question, n);
-            }
-
-            scope.upvote = function(question) {
-                question.hasUpvoted = !question.hasUpvoted;
-                socket.emit('upvoting', question);
-                return QuestionFactory.update(question.id, { upvotes: question.upvotes+1 });
-            }
-
-            scope.downvote = function(question) {
-                question.hasUpvoted = !question.hasUpvoted;
-                socket.emit('downvoting', question);
-                return QuestionFactory.update(question.id, { upvotes: question.upvotes-1 });
-            }
-
-            socket.on('addQuestion', function(question) {
-                scope.questions.unshift(question);
-                if (scope.admin) {
-                    new Notification("New Question", {body: question.text});
-                };
-                scope.$evalAsync();
-            })
-
-            socket.on('deleteQuestion', function(question) {
-                var index = findIndex(question);
-                scope.questions.splice(index, 1);
-                scope.$evalAsync();
-            })
-
-            socket.on('receivedUpvote', function(question) {
-                var index = findIndex(question);
-                scope.questions[index].upvotes = question.upvotes + 1;
-                scope.$evalAsync();
-            })
-
-            socket.on('receivedDownvote', function(question) {
+                rerender();
+            };
+            
+            function renderDownvote(question) {
                 var index = findIndex(question);
                 scope.questions[index].upvotes = question.upvotes - 1;
-                scope.$evalAsync();
-            })
+                rerender();
+            }
 
-            socket.on('moving', function(question, n) {
-                move(question, n);
-                scope.$evalAsync();
-            })
+            function renderUpvote(question) {
+                var index = findIndex(question);
+                scope.questions[index].upvotes = question.upvotes + 1;
+                rerender();
+            }
+
+            function renderDeleteQuestion(question) {
+                var index = findIndex(question);
+                scope.questions.splice(index, 1);
+                rerender();
+            }
+
+            function renderAddQuestion(question) {
+                scope.questions.unshift(question);
+                if (scope.admin) new Notification("New Question", { body: question.text });
+                rerender();
+            }
+
         }
     }
 });
