501b62ce7f9ecfe8cbc4b43f99eedf5746099277
Brinn Riordan <brinn.riordan@gmail.com>
2016-07-08 14:34:58 -0400

    summary page v1

--- browser/js/views/summary/summary.controller.js
+++ browser/js/views/summary/summary.controller.js
@@ -1,6 +1,13 @@
 app.controller('SummaryCtrl', function($scope, SummaryFactory, LectureFactory, lecture) {
 
   $scope.lecture = lecture;
+  // $scope.Series = data2;
+
+  var data2 = {
+  points: [
+    {"x": 1,  "y": 2}
+  ]
+};
 
   LectureFactory.getById($scope.lecture.id)
   .then((lecture)=> {
@@ -9,68 +16,244 @@ app.controller('SummaryCtrl', function($scope, SummaryFactory, LectureFactory, l
 
   SummaryFactory.returnFeedBackJSON($scope.lecture.id)
   .then((feedback) => {
-    $scope.feedback = feedback.map(function(el) {
-      return {"time":el.time-$scope.baseTime, "category":el.category}
-    });
-  });
-
-  function groupyData(feedback) {
-    let grouped = {}
-    feedback.forEach(function(piece) {
-
+    return feedback.map(function(el) {
+      return {"time": el.time-$scope.baseTime, "category":el.category}
     })
+  })
+  .then((feedback)=> {
+    $scope.feedback = feedback
+    return $scope.feedback
+  })
+  .then((feedback) => {
+
+    $scope.timeSeries = timeSeries(feedback);
+
+    $scope.example = {"points": $scope.timeSeries.example};
+    $scope.great = {"points": $scope.timeSeries.great};
+    $scope.confused = {"points": $scope.timeSeries.confused};
+    $scope.see = {"points": $scope.timeSeries.see};
+    $scope.hear = {"points": $scope.timeSeries.hear};
+    $scope.break = {"points": $scope.timeSeries.break};
+    $scope.Series = $scope.great;
+
+    $scope.example_max = $scope.timeSeries.example[$scope.timeSeries.example.length-1].y;
+    $scope.great_max = $scope.timeSeries.great[$scope.timeSeries.great.length-1].y;
+    $scope.confused_max = $scope.timeSeries.confused[$scope.timeSeries.confused.length-1].y;
+    $scope.see_max = $scope.timeSeries.see[$scope.timeSeries.see.length-1].y;
+    $scope.hear_max = $scope.timeSeries.hear[$scope.timeSeries.hear.length-1].y;
+    $scope.break_max = $scope.timeSeries.break[$scope.timeSeries.break.length-1].y;
+
+    let table_data = { "table": [
+      {"category":"Great", "amount":$scope.great_max},
+      {"category":"Confused", "amount":$scope.confused_max},
+      {"category":"Example", "amount":$scope.example_max},
+      {"category":"See", "amount":$scope.see_max},
+      {"category":"Hear", "amount":$scope.hear_max},
+      {"category":"Break", "amount":$scope.break_max}
+    ]};
+    $scope.columns = table_data;
+  })
+
+  function timeSeries(feedback) {
+
+    let max = feedback[feedback.length-1].time;
+
+    let grouped = {
+      "great" : {},
+      "confused": {},
+      "example": {},
+      "see": {},
+      "hear": {},
+      "break": {},
+      "How would you rate this lecture?": {}
+    };
+
+    let timeSeries = {
+      "great" : [],
+      "confused": [],
+      "example": [],
+      "see": [],
+      "hear":[],
+      "break":[],
+      "How would you rate this lecture?": []
+    };
+
+    feedback.forEach(function(piece) {
+      if(!grouped[piece.category][piece.time]) grouped[piece.category][piece.time] = [piece];
+      else grouped[piece.category][piece.time].push(piece);
+    });
+
+    function countResponse(group, type) {
+      let responses = 0;
+      let time = 0;
+      while (time < max) {
+        if (group[type][time]) responses += group[type][time].length;
+        timeSeries[type].push({"x":(time/60), "y":responses});
+        time ++;
+      }
+    }
+
+    countResponse(grouped, "great");
+    countResponse(grouped, "confused");
+    countResponse(grouped, "example");
+    countResponse(grouped, "see");
+    countResponse(grouped, "hear");
+    countResponse(grouped, "break");
+
+    return timeSeries;
   }
 
-    var spec = {
-     "width": 400,
-     "height": 200,
-     "padding": {"top": 10, "left": 30, "bottom": 30, "right": 10},
-     "data": [{"name" : "points"}],
-       "scales": [
-       {
-         "name": "x",
-         "nice": true,
-         "range": "width",
-         "domain": {"data": "points", "field": "data.x"}
-       },
-       {
-         "name": "y",
-         "nice": true,
-         "range": "height",
-         "domain": {"data": "points", "field": "data.y"}
-       }
-     ],
-     "axes": [
-       {"type": "x", "scale": "x"},
-       {"type": "y", "scale": "y"}
-     ],
-     "marks": [
-       {
-         "type": "symbol",
-         "from": {"data": "points"},
-         "properties": {
-           "enter": {
-             "x": {"scale": "x", "field": "data.x"},
-             "y": {"scale": "y", "field": "data.y"},
-             "stroke": {"value": "steelblue"},
-             "fillOpacity": {"value": 0.5}
-           },
-           "update": {
-             "fill": {"value": "transparent"},
-             "size": {"value": 100}
-           },
-           "hover": {
-             "fill": {"value": "pink"},
-             "size": {"value": 300}
-           }
-         }
-       }
-     ]
-    };
-  let data = {"points": $scope.xy}
-  vg.parse.spec(spec, function(chart) {
-    var view = chart({el:".chart", data:data})  //here is where we populate the empty spec data holder with our calculated data
-      .update();
-  });
+  $scope.renderer = 'canvas';
+  // "domain": {"data": "points", "field": "x"},
+  $scope.line = {
+    "width": 800,
+    "height": 200,
+    "padding": {"top": 10, "left": 30, "bottom": 30, "right": 10},
+    "data": [
+      {"name": "points"}
+    ],
+    "scales": [
+      {
+        "name": "x",
+        "type": "linear",
+        "range": "width",
+        "domain": {"data": "points", "field": "x"}
+      },
+      {
+        "name": "y",
+        "range": "height",
+        "nice": true,
+        "domain": {"data": "points", "field": "y"}
+      }
+    ],
+    "axes": [
+      {"type": "x", "scale": "x"},
+      {"type": "y", "scale": "y"}
+    ],
+    "marks": [
+      {
+        "type": "line",
+        "from": {"data": "points"},
+        "key": "x",
+        "properties": {
+          "enter": {
+            "x": {"scale": "x", "field": "x"},
+            "y": {"scale": "y", "field": "y"},
+          },
+          "update": {
+            "x": {"scale": "x", "field": "x"},
+            "y": {"scale": "y", "field": "y"},
+            "stroke": {"value": "steelblue"},
+            "strokeWidth": {"value": "2"}
+          }
+        }
+      }
+    ],
+    "responsive": {
+      "enabled": true
+    }
+  };
+
+  $scope.bar = {
+    "width": 400,
+    "height": 200,
+    "padding": {"top": 10, "left": 30, "bottom": 20, "right": 10},
+
+    "data": [
+      {
+        "name": "table",
+        "values": []
+      }
+    ],
+
+    "signals": [
+      {
+        "name": "tooltip",
+        "init": {},
+        "streams": [
+          {"type": "rect:mouseover", "expr": "datum"},
+          {"type": "rect:mouseout", "expr": "{}"}
+        ]
+      }
+    ],
+
+    "predicates": [
+      {
+        "name": "tooltip", "type": "==",
+        "operands": [{"signal": "tooltip._id"}, {"arg": "id"}]
+      }
+    ],
+
+    "scales": [
+      { "name": "xscale", "type": "ordinal", "range": "width",
+        "domain": {"data": "table", "field": "category"} },
+      { "name": "yscale", "range": "height", "nice": true,
+        "domain": {"data": "table", "field": "amount"} }
+    ],
+
+    "axes": [
+      { "type": "x", "scale": "xscale" },
+      { "type": "y", "scale": "yscale" }
+    ],
+
+    "marks": [
+      {
+        "type": "rect",
+        "from": {"data":"table"},
+        "properties": {
+          "enter": {
+            "x": {"scale": "xscale", "field": "category"},
+            "width": {"scale": "xscale", "band": true, "offset": -1},
+            "y": {"scale": "yscale", "field": "amount"},
+            "y2": {"scale": "yscale", "value":0}
+          },
+          "update": { "fill": {"value": "steelblue"} },
+          "hover": { "fill": {"value": "red"} }
+        }
+      },
+      {
+        "type": "text",
+        "properties": {
+          "enter": {
+            "align": {"value": "center"},
+            "fill": {"value": "#333"}
+          },
+          "update": {
+            "x": {"scale": "xscale", "signal": "tooltip.category"},
+            "dx": {"scale": "xscale", "band": true, "mult": 0.5},
+            "y": {"scale": "yscale", "signal": "tooltip.amount", "offset": -5},
+            "text": {"signal": "tooltip.amount"},
+            "fillOpacity": {
+              "rule": [
+                {
+                  "predicate": {"name": "tooltip", "id": {"value": null}},
+                  "value": 0
+                },
+                {"value": 1}
+              ]
+            }
+          }
+        }
+      }
+    ]
+  }
+
+  // default scale definition
+  $scope.line.width = 0.55*window.innerWidth;
+  $scope.line.height = 0.25*window.innerHeight;
+  $scope.bar.width = 0.25*window.innerWidth;
+  $scope.bar.height = 0.25*window.innerHeight;
+
+  // rerender on resize
+  function chart_render() {
+    $scope.line.width = 0.55*window.innerWidth;
+    $scope.line.height = 0.25*window.innerHeight;
+    $scope.bar.width = 0.25*window.innerWidth;
+    $scope.bar.height = 0.25*window.innerHeight;
+
+    $scope.$digest();
+  }
+
+  window.onresize = chart_render;
 
 });
